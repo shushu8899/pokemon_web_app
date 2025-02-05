836a8b09388fb08d64b18e9992cf2ec6dc5ec4bb
KierenChua2023 <kieren.chua.2023@mitb.smu.edu.sg>
2025-02-06 00:27:25 +0800

    Added basic DB materials

---
+++ app/models/auction.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+"""
+Auction Table - Will contain all the Auction data
+"""
+
+from sqlalchemy import Column, Integer, VARCHAR, Float, ForeignKey, DateTime
+from app.db.db import Base
+from pydantic import BaseModel, ConfigDict
+from sqlalchemy.orm import relationship
+
+
+class Auction(Base):
+    __tablename__ = "auctions"
+
+    AuctionID = Column(Integer, primary_key=True, index=True)
+    CardID = Column(
+        Integer, ForeignKey("cards.CardID", ondelete="CASCADE"), nullable=False
+    )
+    SellerID = Column(
+        Integer, ForeignKey("cards.OwnerID", ondelete="CASCADE"), nullable=False
+    )
+
+    MinimumIncrement = Column(Float, nullable=False, default=0.01)
+    Status = Column(VARCHAR, nullable=False, default="In Progress")
+    EndTime = Column(DateTime, nullable=False)
+
+    HighestBidderID = Column()
