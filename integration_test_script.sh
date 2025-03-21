a7c645a1e9a7ea85ed824cf496ad777195330452
KierenChua2023 <kieren.chua.2023@mitb.smu.edu.sg>
2025-03-21 23:37:29 +0800

    sample seed

---
+++ integration_test_script.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Enable error handling
+set -e
+
+# Move to the directory of the script
+cd "$(dirname "$0")"
+
+echo "Deleting migrations folder..."
+rm -rf migrations
+
+echo "Initializing Alembic migrations..."
+alembic init migrations
+
+ENV_FILE="migrations/env.py"
+TEMP_FILE="temp.txt"
+
+# Set the search term and replacement block
+SEARCH_TERM="target_metadata = None"
+REPLACEMENT=$(cat <<EOF
+from app.db.db import Base
+from app.models.auction import Auction
+from app.models.card import Card
+from app.models.profile import Profile
+from app.models.notifications import Notification
+target_metadata = Base.metadata
+EOF
+)
+
+# Replace the specific line in env.py
+awk -v search="$SEARCH_TERM" -v replacement="$REPLACEMENT" '
+{
+    if ($0 == search) {
+        print replacement
+    } else {
+        print $0
+    }
+}' "$ENV_FILE" > "$TEMP_FILE" && mv "$TEMP_FILE" "$ENV_FILE"
+
+echo "Creating Alembic revision for Auction App..."
+alembic revision --autogenerate -m "Init Auction App"
+
+echo "Applying migrations..."
+alembic upgrade head
+
+echo "Starting FastAPI application with uvicorn..."
+uvicorn app.main:app --reload
